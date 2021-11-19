faa48ff5d54ef44ccd68de0baf1d93fd2848c749
dusanmila <milasinovicdusan@gmail.com>
2021-11-19 12:27:46 +0100

    Mesecni basic crud operations

---
+++ src/main/java/frikom/repos/MesecniRepository.java
@@ -0,0 +1,10 @@
+package frikom.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import frikom.jpa.Mesecni;
+
+public interface MesecniRepository  extends JpaRepository<Mesecni, Integer> {
+
+}
