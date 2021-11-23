75777b6bab4b2d23695307dab4261e32b302fa63
dusanmila <milasinovicdusan@gmail.com>
2021-11-23 16:35:47 +0100

    Spring security i jwt token

---
+++ src/main/java/frikom/models/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package frikom.models;
+
+public class AuthenticationResponse {
+
+	private final String jwt;
+	
+	public AuthenticationResponse(String jwt)
+	{
+		this.jwt=jwt;
+	}
+	public String getJwt()
+	{
+		return jwt;
+	}
+}
